9f1fd2cf94ff9333366da385d8b25b6a50fdf2a5
Luigi Donadel <luigi.donadel@gmail.com>
2016-03-13 14:20:35 +0100

    added basic models

---
+++ api/models/User.js
@@ -0,0 +1,23 @@
+/**
+* User.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+      username: "string",
+      games: {collection: "game"}
+  },
+  getRandom() {
+      var self = this;
+      return this.count().then((value) => {
+          var random = Math.floor(Math.random() * value);
+          if(random < 0) random = 0;
+
+          return self.find({skip: random, limit: 1});
+      });
+  }
+};
